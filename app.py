375e07b018ac5ca2c5820837b8362cac090c0ad4
acramer20 <97561089+acramer20@users.noreply.github.com>
2022-03-07 14:15:43 -1000

    adding to repo

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, session, redirect, render_template, jsonify, request 
+from boggle import Boggle
+
+app = Flask(__name__)
+app.confid["SECRET_KEY"] = "asjbfakjsk"
+
+boggle_game = Boggle()
+
+@app.route('/')
+def homepage():
+    """Showing the board"""
+    board = boggle_game.make_board()
+    session['board']=[]
+    return render_template('index.html')
+
+
+
