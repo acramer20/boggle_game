ed1ba2d41f23e2cfee8dd3a4c671bcf62fa94c3f
acramer20 <97561089+acramer20@users.noreply.github.com>
2022-03-09 15:28:15 -1000

    updating

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, session, redirect, render_template, jsonify, request 
 from boggle import Boggle
 
@@ -17,8 +18,23 @@ def homepage():
 
 @app.route("/check-word")
 def check_word():
+    """checking if word is in dictionary/board"""
     word = request.args['word']
     board = session['board']
     response = boggle_game.check_valid_word(board, word)
     return jsonify({"result": response})
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+    """recording the score, num of times played, and updating the highscore if reached"""
+    score = request.json["score"]
+    num_plays = session.get("num_plays", 0)
+    highscore = session.get("highscore", 0)
+
+    session['num_plays'] = num_plays + 1
+    session['highscore'] = max(score, highscore)
+    
+    return jsonify(brokeRecord=score > highscore)
+
+
 
--- static/app.js
+++ static/app.js
@@ -1,27 +1,36 @@
-//ask for clarification on how to 
+
 
 class BoggleGame {
     constructor (boardId, secs =60) {
         this.secs = secs;
-        // this.showTimer()
+        this.showTimer()
 
         this.score = 0;
         this.words = new Set();
         this.board = $("#" + boardId); 
 
+
+        this.timer = setInterval(this.tick.bind(this), 1000);
+
         $(".guess", this.board).on("submit", this.handleSubmit.bind(this)); 
     }
+
+    /*showing word in a list of accepted words */
     showWord(word) {
         $(".guess", this.board).append($("<li>", {text: word}));
     }
-
+    /* rendering a message to inform player of status of word*/
     showMessage(msg, cls) {
         $(".msg", this.board).text(msg).removeClass().addClass(`msg ${cls}`)
+    }
+    /* updates the score as words are added to the list */
+    showScore(){
+        $(".score", this.board).text(this.score)
     }
 
    
 
-
+    /* handling click event. Once submit is clicked, check if word is valid for the words in the board */
     async handleSubmit(evt) {
         evt.preventDefault();
         const $word = $(".word", this.board);
@@ -33,6 +42,8 @@ class BoggleGame {
             this.showMessage(`Already found ${word}`, "err");
         return;
         }
+
+        /* reaching out to server to check word */
         const resp = await axios.get("/check-word", {params: {word: word}});
         if (resp.data.result === 'not-word'){
             this.showMessage(`${word} is not a valid English word`, 'err');
@@ -42,11 +53,39 @@ class BoggleGame {
         }
         else {
             this.showWord(word);
-            // this.score += word.length
-            // this.showScore();
+            this.score += word.length
+            this.showScore();
             this.words.add(word);
             this.showMessage(`Added ${word}`, 'ok');
         }
         $word.val('').focus();
+    }
+    /* function to update time left*/
+    showTimer(){
+        $(".timer", this.board).text(this.secs)
+    }
+
+    // could use some help understanding why this is async below
+    /* tick function to lower the secs remaining and clear timer when 0 is reached*/
+    async tick() {
+        this.secs -= 1
+        this.showTimer()
+
+        if (this.secs === 0){
+            clearInterval(this.timer)
+            await this.scoreGame()
+        }
+    }
+    /* post score at end of game */
+    async scoreGame() {
+        $(".guess", this.board).hide()
+        const resp = await axios.post("/post-score", {score: this.score});
+        if (resp.data.brokeRecord) {
+            this.showMessage(`Heres your new record: ${this.score}`, "ok");
+        }
+        else {
+            this.showMessage(`Final score: ${this.score}`, "ok")
+        }
+        
     }
     }
